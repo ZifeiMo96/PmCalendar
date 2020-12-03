5ea7d366bb9eea30c9a2965dcf7a87e72bed78f3
Mozifei96 <1927870057@qq.com>
2020-12-03 23:11:25 +0800

    init：确定框架

---
+++ PmCalendar/PmCalendar/MainPage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PmService;
+
+
+namespace PmCalendar
+{
+    public partial class MainPage : Form
+    {
+        
+        public MainPage()
+        {
+            InitializeComponent();
+            
+        }
+
+    }
+}
---
+++ PmCalendar/PmService/Service.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PmService
+{
+    public class Service
+    {
+        /// <summary>
+        /// 存储训练数据
+        /// </summary>
+        public List<PmData> DataList { get; set; }
+    }
+}
